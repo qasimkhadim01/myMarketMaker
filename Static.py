6bf7727e65b23f8062b0b6952370821c272d1512
qasimkhadim01 <91701823+qasimkhadim01@users.noreply.github.com>
2024-04-14 16:37:42 +0100

    Delete Static.py

--- Static.py
+++
@@ -1,5 +0,0 @@
-myApiKeyObsolete = "f4a64af3ff9c584d39395fa1c4477026"
-mySecretKeyObsolete = "2c70a4f598daf7feb6c60fdfdb6a9834549933fba65e21ded9cb1389558269b2"
-gateioCollectorFile = "c:\\Users\\qasim\\Projects\\tickdata\\gateio\\"
-orderCounter=0
-logFile = "C:\\Users\\qasim\\Projects\\temp\\UMEE.log"
