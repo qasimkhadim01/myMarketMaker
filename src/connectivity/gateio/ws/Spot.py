182f941ff1a96b6deb3a790e8b7db1e14178c509
qasimkhadim01 <91701823+qasimkhadim01@users.noreply.github.com>
2024-04-14 18:47:33 +0300

    Add files via upload

---
+++ src/connectivity/gateio/ws/Spot.py
@@ -0,0 +1,57 @@
+from connectivity.gateio.ws.Client import BaseChannel
+
+
+class SpotTickerChannel(BaseChannel):
+    name = 'spot.tickers'
+
+
+class SpotPublicTradeChannel(BaseChannel):
+    name = 'spot.trades'
+
+
+class SpotCandlesticksChannel(BaseChannel):
+    name = 'spot.candlesticks'
+
+
+class SpotBookTickerChannel(BaseChannel):
+    name = 'spot.book_ticker'
+
+
+class SpotOrderBookUpdateChannel(BaseChannel):
+    name = 'spot.order_book_update'
+
+class SpotOrderPlaceChannel(BaseChannel):
+    name = 'spot.order_place'
+    require_auth = True
+class SpotOrderBookChannel(BaseChannel):
+    name = 'spot.order_book'
+
+
+class SpotOrderChannel(BaseChannel):
+    name = 'spot.orders'
+    require_auth = True
+
+
+class SpotUserTradesChannel(BaseChannel):
+    name = 'spot.usertrades'
+    require_auth = True
+
+
+class SpotBalanceChannel(BaseChannel):
+    name = 'spot.balances'
+    require_auth = True
+
+
+class SpotMarginBalanceChannel(BaseChannel):
+    name = 'spot.margin_balances'
+    require_auth = True
+
+
+class SpotFundingBalanceChannel(BaseChannel):
+    name = 'spot.funding_balances'
+    require_auth = True
+
+
+class SpotCrossMarginBalanceChannel(BaseChannel):
+    name = 'spot.cross_balances'
+    require_auth = True
---
+++ src/connectivity/gateio/ws/__init__.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+from connectivity.gateio.ws.Client import Configuration
+from connectivity.gateio.ws.Client import Connection
+from connectivity.gateio.ws.Client import GateWebsocketError
+from connectivity.gateio.ws.Client import WebSocketResponse
+
+__all__ = [Configuration, Connection, GateWebsocketError, WebSocketResponse]
+
+#python -m tickcollector.LocalOrderBook
