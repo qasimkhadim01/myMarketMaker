90f6e7cc120e8c1f00bd2e52de5d77ae7214ddcb
qasimkhadim01 <91701823+qasimkhadim01@users.noreply.github.com>
2024-05-01 09:37:57 +0300

    Add files via upload

--- src/core/Instrument.py
+++ src/core/Instrument.py
@@ -2,19 +2,22 @@ class Coin:
     UX = "UMEE"
     USDT = "USDT"
     BTC = "BTC"
-
+    XRP = "XRP"
 
 class Instrument:
-    def __init__(self, base, counter):
+    def __init__(self, base, counter, amountPrecision = 2, pricePrecision =5 ):
         self.base = base
         self.counter = counter
+        self.amountPrecision = amountPrecision
+        self.pricePrecision = pricePrecision
 
     def __str__(self):
         return str(self.base + "_" + self.counter).upper()
 
 
 class Instruments:
-    instruments = {"UMEE_USDT": Instrument(Coin.UX, Coin.USDT),
-                   "BTC_USDT": Instrument(Coin.BTC, Coin.USDT),
-                   "USDT_USDT": Instrument(Coin.BTC, Coin.USDT)
+    instruments = {"UMEE_USDT": Instrument(Coin.UX, Coin.USDT, 6, 6),
+                   "BTC_USDT": Instrument(Coin.BTC, Coin.USDT,  6, 2),
+                   "XRP_USDT": Instrument(Coin.XRP, Coin.USDT),
+                   "USDT_USDT": Instrument(Coin.USDT, Coin.USDT)
                    }
--- src/core/Orders.py
+++ src/core/Orders.py
@@ -1,11 +1,18 @@
+import logging
 from datetime import datetime
 from decimal import Decimal
-
+import time
+from connectivity.gateio import Api
 from core.Instrument import Instrument
 from core.MyEnums import OrderSide, OrderStatus, Role
+from marketmaker.Strategy import Strategy
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class Order:
+    orderCounter = 0
+
     def __init__(self, id: str, instrument: Instrument, side: OrderSide, amount: Decimal, price: Decimal):
         self.timestamp = datetime.now()
         self.id = id
@@ -14,31 +21,43 @@ class Order:
         self.amount = amount
         self.price = Decimal(price)
         self.status = OrderStatus.New
+        self.filledAmount = 0
+        self.strategy:Strategy = None
+        self.creationTime = int(time.time())
+        self.completionTime =int(time.time())
 
     def __eq__(self, other):
         return self.price == other.price and self.side == other.side
 
     def __str__(self):
-        return '(id=%s, instrument=%s, price=%s, amount=%s, %s)' % (self.id, self.instrument, self.price, self.amount, self.side)
+        return (f"{self.timestamp.strftime('%H:%M:%S')},id={self.id}, {str(self.instrument)}, price={self.price:.{self.instrument.pricePrecision}f}, amount={self.amount:.{self.instrument.amountPrecision}f}"
+                f", filledAmount={self.filledAmount:.{self.instrument.amountPrecision}f}, {self.side}, {self.status}, {self.strategy.__class__.__name__}, {self.__class__.__name__}")
+    @staticmethod
+    def nextOrderId() -> str:
+        orderId = "t-" + Api.subAccount + "_" + str(Order.orderCounter)
+        Order.orderCounter += 1
+        return orderId
 
 
 class SpotLimitOrder(Order):
-    def __init__(self, id: str, instrument: Instrument, side: OrderSide, amount: Decimal, price: Decimal):
+    def __init__(self, id: str, instrument: Instrument, side: OrderSide, amount: Decimal, price: Decimal, strategy: Strategy):
         super().__init__(id, instrument, side, amount, Decimal(price))
-
-
+        self.strategy = strategy
 class SpotMarketOrder(Order):
     def __init__(self, id: str, instrument: Instrument, side: OrderSide, amount: Decimal, price: Decimal):
         super().__init__(id, instrument, side, amount, Decimal(price))  # price must be marketPrice.
 
+
 class FilledOrder(Order):
-    def __init__(self, id: str, instrument: Instrument, side: OrderSide, filledAmount: Decimal, filledPrice: Decimal, role: Role):
+    def __init__(self, id: str, instrument: Instrument, side: OrderSide, filledAmount: Decimal, filledPrice: Decimal,
+                 role: Role):
         super().__init__(id, instrument, side, filledAmount, Decimal(filledPrice))
         self.status = OrderStatus.Filled
         self.role = role
+        self.filledAmount = filledAmount
 
     def getSignedAmount(self):
-        return self.amount if self.side == OrderSide.Buy else self.amount*-1
+        return self.amount if self.side == OrderSide.Buy else self.amount * -1
 
     def getQuoteSignedAmount(self):
         return self.price * self.amount * -1 if self.side == OrderSide.Buy else self.price * self.amount
@@ -53,4 +72,4 @@ class TickEvent:
 
     @property
     def midPrice(self):
-        return (self.bidPrice + self.askPrice)/Decimal(2.0)
+        return (self.bidPrice + self.askPrice) / Decimal(2.0)
